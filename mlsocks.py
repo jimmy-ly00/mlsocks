fc3db0823b7abe64553550faeb2fc2996f1e0c2f
Jimmy <jimmy.ly08@gmail.com>
2017-07-24 13:42:15 +0100

    fixed delay for r more incoming stream that FIONREAD did not count for

--- mlsocks.py
+++ mlsocks.py
@@ -12,8 +12,8 @@ import array
 import time
 import random
 from math import log, exp, ceil
-from gevent import sleep, spawn, joinall, wait, select
-from gevent.pool import Pool
+from gevent import sleep, spawn, select
+from gevent.pool import Group
 
 
 BUF_SIZE = 512
@@ -35,19 +35,18 @@ class Socks5Server(SocketServer.StreamRequestHandler):
 
         timestamp = time.time() * 1000
         new = timestamp + delay * 1000
-
         # Stops messages overtaking each other by checking against the latest delay
         if prev[0] < new:
             prev[0] = new
             prev[1] = delay
         else:
             delay = prev[1]
-        return delay
 
+        return delay
+        
     def handle_tcp(self, sock, remote):
         fdset = [sock, remote]
-        sock_tasks = Pool(500)
-        remote_tasks = Pool(500)
+        tasks = Group()
         prev = [0, 0]
         sock_switch = remote_switch = 0
         sock_counter = remote_counter = 0
@@ -57,46 +56,41 @@ class Socks5Server(SocketServer.StreamRequestHandler):
         while True:
             r, w, e = select.select(fdset, [], [])
             # Problem is knowing beforehand when the socket is going to switch to joinall remainding tasks
-            # This will check size of available bytes of the socket to notify the last send()/recv()
+            # FIONREAD will check size of available bytes of the socket to catch the last send()/recv()
             if sock in r:
                 if sock_switch == 0:
                     fcntl.ioctl(sock, termios.FIONREAD, sock_size, True)
                     sock_count = ceil(sock_size[0] / float(BUF_SIZE))
                     print("sock", sock_size[0], sock_count)
+                    sock_switch = 1
+                delay = self.calculate_delay(prev)
+                sock_buf = sock.recv(BUF_SIZE)
+                #print(sock_buf)
+                if sock_buf is None or sock_buf == "": break
+                tasks.spawn(self.delay_message, sock_buf, delay, remote)
+                sock_counter += 1
             if remote in r:
                 if remote_switch == 0:
                     fcntl.ioctl(remote, termios.FIONREAD, remote_size, True)
                     remote_count = ceil(remote_size[0] / float(BUF_SIZE))
                     print("remote", remote_size[0], remote_count)
-
-            # Spawn delayed task for socket
-            if sock in r:
-                sock_switch = 1
-                delay = self.calculate_delay(prev)
-                sock_buf = sock.recv(BUF_SIZE)
-                if sock_buf is None or sock_buf == "": break
-                #print(sock_buf)
-                sock_tasks.spawn(self.delay_message, sock_buf, delay, remote)
-                sock_counter += 1
-            if remote in r:
-                remote_switch = 1
+                    remote_switch = 1
                 delay = self.calculate_delay(prev)
                 remote_buf = remote.recv(BUF_SIZE)
-                if remote_buf is None or remote_buf == "": break
                 #print(remote_buf)
-                remote_tasks.spawn(self.delay_message, remote_buf, delay, sock)
+                if remote_buf is None or remote_buf == "": break
+                tasks.spawn(self.delay_message, remote_buf, delay, sock)
                 remote_counter += 1
 
             # Wait for last task before switching socket
-            if sock_count == sock_counter:
+            if sock_count == sock_counter and sock_switch == 1:
                 print("joiningsocks")
-                joinall(sock_tasks, raise_error=True)
-                sock_counter = 0
-            if remote_count == remote_counter:
+                tasks.join()
+                sock_counter = sock_switch = 0
+            if remote_count == remote_counter and remote_switch == 1:
                 print("joiningremote")
-                joinall(remote_tasks, raise_error=True)
-                remote_counter = 0
-
+                tasks.join()
+                remote_counter = remote_switch = 0
 
     def handle(self):
         try:
